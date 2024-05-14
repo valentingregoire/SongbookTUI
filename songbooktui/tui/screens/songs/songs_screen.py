7a7cacec5b7a275648ccbf6f0f29b6711bc80358
valentingregoire <valentin.gregoire@gmail.com>
2024-05-14 22:31:01 +0200

    fix song_screen previous and next buttons

--- songbooktui/tui/screens/songs/songs_screen.py
+++ songbooktui/tui/screens/songs/songs_screen.py
@@ -1,5 +1,5 @@
 from textual.app import ComposeResult
-from textual.containers import Vertical
+from textual.containers import Vertical, VerticalScroll
 from textual.coordinate import Coordinate
 from textual.reactive import reactive
 from textual.screen import Screen
@@ -26,48 +26,49 @@ class SongsScreen(Screen):
         return self.songs.get(self.current_song_id)
 
     def compose(self) -> ComposeResult:
-        table = DataTable(id="songs-table", classes="w-auto")
-        table.add_columns("  Title", "󰙃  Artist", "󰽰 ", "󰟚 ", " ")
-        table.cursor_type = "row"
-        table.border_title = "Songs"
-        yield table
-        details_container = Vertical(id="song-details-container", classes="h-1fr")
-        details_container.border_title = "Details"
-        with details_container:
-            txt_id = Input(id="txt_id", name="id", placeholder="ID", disabled=True)
-            txt_id.border_title = "ID"
-            yield txt_id
-            txt_title = Input(
-                # self.current_song.title,
-                id="txt_title",
-                name="title",
-                placeholder="Title",
+        with VerticalScroll():
+            table = DataTable(id="songs-table", classes="w-auto")
+            table.add_columns("  Title", "󰙃  Artist", "󰽰 ", "󰟚 ", " ")
+            table.cursor_type = "row"
+            table.border_title = "Songs"
+            yield table
+            details_container = Vertical(id="song-details-container", classes="h-1fr")
+            details_container.border_title = "Details"
+            with details_container:
+                txt_id = Input(id="txt_id", name="id", placeholder="ID", disabled=True)
+                txt_id.border_title = "ID"
+                yield txt_id
+                txt_title = Input(
+                    # self.current_song.title,
+                    id="txt_title",
+                    name="title",
+                    placeholder="Title",
+                )
+                txt_title.border_title = "  Title"
+                yield txt_title
+                txt_artist = Input(id="txt_artist", name="artist", placeholder="Artist")
+                txt_artist.border_title = "󰙃  Artist"
+                yield txt_artist
+                txt_key = Input(id="txt_key", name="key", placeholder="Key")
+                txt_key.border_title = "󰽰 Key"
+                yield txt_key
+                txt_bpm = Input(id="txt_bpm", name="bpm", placeholder="BPM")
+                txt_bpm.border_title = "󰟚 BPM"
+                yield txt_bpm
+                txt_duration = Input(
+                    id="txt_duration", name="duration", placeholder="Duration"
+                )
+                txt_duration.border_title = "  Duration"
+                yield txt_duration
+            yield WidgetFactory.actions_bar(
+                [
+                    ActionButton(
+                        " Previous", action="screen.previous", classes="btn-link"
+                    ),
+                    WidgetFactory.btn_save(),
+                    ActionButton("Next ", action="screen.next", classes="btn-link"),
+                ]
             )
-            txt_title.border_title = "  Title"
-            yield txt_title
-            txt_artist = Input(id="txt_artist", name="artist", placeholder="Artist")
-            txt_artist.border_title = "󰙃  Artist"
-            yield txt_artist
-            txt_key = Input(id="txt_key", name="key", placeholder="Key")
-            txt_key.border_title = "󰽰 Key"
-            yield txt_key
-            txt_bpm = Input(id="txt_bpm", name="bpm", placeholder="BPM")
-            txt_bpm.border_title = "󰟚 BPM"
-            yield txt_bpm
-            txt_duration = Input(
-                id="txt_duration", name="duration", placeholder="Duration"
-            )
-            txt_duration.border_title = "  Duration"
-            yield txt_duration
-        yield WidgetFactory.actions_bar(
-            [
-                ActionButton(
-                    " Previous", action="screen.previous", classes="btn-link"
-                ),
-                WidgetFactory.btn_save(),
-                ActionButton("Next ", action="screen.next", classes="btn-link"),
-            ]
-        )
 
     async def on_mount(self) -> None:
         await self.populate_form()
@@ -75,17 +76,25 @@ class SongsScreen(Screen):
 
     async def action_previous(self) -> None:
         table = self.query_one(DataTable)
-        row_index = table.cursor_row if table.cursor_row >= 0 else len(table.rows) - 1
-        self.current_song_id = self.songs[row_index].id
-        await self.populate_form()
-        await self.populate_songs_table(
-            row_index - 1 if row_index > 0 else len(table.rows) - 1
+        row_index = (
+            table.cursor_row - 1 if table.cursor_row > 0 else len(table.rows) - 1
         )
+        self.current_song_id = int(
+            table.coordinate_to_cell_key(Coordinate(row_index, 0)).row_key.value
+        )
+        await self.populate_form()
+        await self.populate_songs_table(row_index)
 
     async def action_next(self) -> None:
-        self.current_song_id = (self.current_song_id + 1) % len(self.songs)
+        table = self.query_one(DataTable)
+        row_index = (
+            table.cursor_row + 1 if table.cursor_row < len(table.rows) - 1 else 0
+        )
+        self.current_song_id = int(
+            table.coordinate_to_cell_key(Coordinate(row_index, 0)).row_key.value
+        )
         await self.populate_form()
-        await self.populate_songs_table()
+        await self.populate_songs_table(row_index)
 
     async def populate_form(self) -> None:
         self.query_one("#txt_id", Input).value = str(self.current_song.id)
